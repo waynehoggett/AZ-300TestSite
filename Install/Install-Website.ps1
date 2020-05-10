cd3d58230a7df125fa9a9f8b1835af5eca134ae5
Wayne Hoggett <waynehoggett@hotmail.com>
2020-05-10 11:29:44 +1000

    Updated install script

--- Install/Install-Website.ps1
+++ Install/Install-Website.ps1
@@ -1,2 +1,18 @@
 #Install IIS
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+#Add required types
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+#Download website
+$url = "https://github.com/waynehoggett/AZ-300TestSite/raw/master/Install/backup.zip"
+$zipfile = "C:\backup.zip"
+$folder = "C:\inetpub\wwwroot"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -uri $url -outfile $zipfile
+
+#Extract and install website
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $folder)
+
+#Clean-up
+Remove-Item -Path $zipfile
